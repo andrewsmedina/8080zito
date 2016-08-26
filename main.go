e24479357753386060a770f00649aa6e8893c600
Andrews Medina <andrewsmedina@gmail.com>
2016-08-26 00:45:12 -0300

    using switch and print missing codes

--- main.go
+++ main.go
@@ -10,42 +10,35 @@ import (
 func disassemble(b []byte, pc int) int {
 	var op int = 1
 	code := b[pc]
-	if code == 0x00 {
+	switch code {
+	case 0x00:
 		fmt.Println("NOP")
-	}
-	if code == 0x01 {
+	case 0x01:
 		fmt.Printf("LXI    B,#$%02x%02x\n", b[pc+2], b[pc+1])
 		op = 3
-	}
-	if code == 0x02 {
+	case 0x02:
 		fmt.Println("STAX   B")
-	}
-	if code == 0x03 {
+	case 0x03:
 		fmt.Println("INX    B")
-	}
-	if code == 0x04 {
+	case 0x04:
 		fmt.Println("INR    B")
-	}
-	if code == 0x05 {
+	case 0x05:
 		fmt.Println("DCR    B")
-	}
-	if code == 0x06 {
+	case 0x06:
 		fmt.Printf("MVI    B,#$%02x\n", b[pc+1])
 		op = 2
-	}
-	if code == 0x07 {
+	case 0x07:
 		fmt.Println("RLC")
-	}
-	if code == 0x08 {
+	case 0x08:
 		fmt.Println("NOP")
-	}
-	if code == 0x3e {
+	case 0x3e:
 		fmt.Printf("MVI    A,#0x%02x\n", b[pc+1])
 		op = 2
-	}
-	if code == 0xc3 {
+	case 0xc3:
 		fmt.Printf("JMP    $%02x%02x\n", b[pc+2], b[pc+1])
 		op = 3
+	default:
+		fmt.Printf("MISSING  %x\n", code)
 	}
 	return op
 }
