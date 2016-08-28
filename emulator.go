67c8d1521887cbbd529017291ec0db7c4361a60f
Andrews Medina <andrewsmedina@gmail.com>
2016-08-28 20:57:39 -0300

    add emulator state

--- emulator.go
+++ emulator.go
@@ -9,6 +9,21 @@ type conditionCodes struct {
 	pad int
 }
 
+type state struct {
+	a      int
+	b      int
+	c      int
+	d      int
+	e      int
+	h      int
+	l      int
+	sp     int
+	pc     int
+	memory int
+	cc     conditionCodes
+	enable bool
+}
+
 func emulator(b []byte, pc int) int {
 	var op int = 1
 	return op
