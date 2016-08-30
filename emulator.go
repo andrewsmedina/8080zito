177755c6accd96cfab43c72e35f936508e782d8b
Andrews Medina <andrewsmedina@gmail.com>
2016-08-29 23:02:26 -0300

    emulator: implement move ops for b register

--- emulator.go
+++ emulator.go
@@ -29,6 +29,22 @@ func emulator(s *state, b []byte) int {
 	op := 1
 	code := b[s.pc]
 	switch code {
+	case 0x01:
+		s.c = int(b[s.pc+1])
+		s.b = int(b[s.pc+2])
+		op = 3
+	case 0x41:
+		s.b = s.c
+	case 0x42:
+		s.b = s.d
+	case 0x43:
+		s.b = s.e
+	case 0x44:
+		s.b = s.h
+	case 0x45:
+		s.b = s.l
+	case 0x47:
+		s.b = s.a
 	default:
 		fmt.Printf("MISSING  %x\n", code)
 	}
